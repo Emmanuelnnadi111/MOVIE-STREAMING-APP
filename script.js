5a8fdab86c873339b4b2c522c28df7028787f234
Emmanuelnadi111 <doneonyi920@gmail.com>
2023-10-27 22:55:02 +0100

    solved the not searching issue

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const btn = document.querySelector('.search');
 
 const image_path = "https://image.tmdb.org/t/p/w1280/";
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=18268bc1582caba55140211544f61c54';
-
-const search_url = `https://api.themoviedb.org/3/search/movie?api_key=18268bc1582caba55140211544f61c54&query=${input.value}`;
+// https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=18268bc1582caba55140211544f61c54
 
 btn.addEventListener('click', searchMovies);
 
@@ -15,18 +14,20 @@ function searchMovies() {
                   input.style.borderColor = 'red';
                   alert('Input field Required');
             } else {
-                getMovies();
+                getMovies(input.value);
                 input.value = '';
             }
             getMovies(input.value);
 }
-async function getMovies() {
-   const response = await fetch(API_URL);
+async function getMovies(input) {
+const search_url = `https://api.themoviedb.org/3/search/movie?api_key=18268bc1582caba55140211544f61c54&query=${input}`;
+   const response = await fetch(search_url);
    const data = await response.json();
    displayMovies(data.results);
    console.log(data.results);
    return data.results;
 }
+
 
 function displayMovies (movies) {  
     
@@ -38,7 +39,7 @@ function displayMovies (movies) {
                          <div class="output bg-white  shadow-2xl rounded-2xl p-2">
                          <img src="${image_path  + poster_path}" alt="${title}" class="image-hover w-[100%]">
                                 <div class="overview rounded-2xl p-2">
-                                    <h2 class="text-center font-bold text-blue-800  text-5xl">${title}</h2>
+                                    <h2 class="text-center font-bold text-blue-800  text-2xl">${title}</h2>
                                     <h2 class="font-bold text-center ">Overview</h2>
                                     <p class="text-center ">${overview}</p>
                                 </div>
@@ -48,60 +49,3 @@ function displayMovies (movies) {
     });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function showMovie(data) {
-//     data.forEach((data) => {
-//         const {title, poster_path, vote_average, overview} = myMovies;  
-//                     let movieContainer = '';  
-//                      movieContainer +=`
-//                      <div class="wrap">
-//                          <div class="output bg-white  shadow-2xl rounded-2xl p-2">
-//                          <img src="${image_path + poster_path}" alt="${title}" class="image-hover w-[100%]">
-//                                 <div class="overview rounded-2xl p-2">
-//                                     <h2 class="text-center font-bold text-blue-800  text-5xl">${title}</h2>
-//                                     <h2 class="font-bold text-center ">Overview</h2>
-//                                     <p class="text-center ">${overview}</p>
-//                                 </div>
-//                         </div>
-//                      </div>`;
-//                 document.querySelector('.output-container').innerHTML = movieContainer; 
-//     })
-// }
-// function displayMovies (myMovies) { 
-    
-//          const {title, poster_path, vote_average, overview} = myMovies;  
-//             let movieContainer = '';  
-//              movieContainer +=`
-//              <div class="wrap">
-//                  <div class="output bg-white  shadow-2xl rounded-2xl p-2">
-//                  <img src="${image_path + poster_path}" alt="${title}" class="image-hover w-[100%]">
-//                         <div class="overview rounded-2xl p-2">
-//                             <h2 class="text-center font-bold text-blue-800  text-5xl">${title}</h2>
-//                             <h2 class="font-bold text-center ">Overview</h2>
-//                             <p class="text-center ">${overview}</p>
-//                         </div>
-//                 </div>
-//              </div>`;
-//         document.querySelector('.output-container').innerHTML = movieContainer;    
-  
-// }
