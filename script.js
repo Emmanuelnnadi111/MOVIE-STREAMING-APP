9daefb94dfb75e23a79871dae6388929af4ee0b9
Emmanuelnadi111 <doneonyi920@gmail.com>
2023-11-08 23:27:29 +0100

    added a loader

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.getElementById('getMovies');
 const btn = document.querySelector('.search');
+const loadingImage = document.querySelector('.load');
 
 const image_path = "https://image.tmdb.org/t/p/w1280/";
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=18268bc1582caba55140211544f61c54';
@@ -13,27 +14,39 @@ function searchMovies() {
             if (input.value == '') {
                   input.style.borderColor = 'red';
                   alert('Input field Required');
-            } else {
-                getMovies(input.value);
-                input.value = '';
+            } else  {
+                    loadingImage.classList.remove('hidden');
+                   getMovies(input.value);
+                    input.value = '';
+
             }
-            getMovies(input.value);
+            getMovies(input.value).catch((error) => {
+
+                console.log('Error:', error);
+            })
+            
 }
 async function getMovies(input) {
-const search_url = `https://api.themoviedb.org/3/search/movie?api_key=18268bc1582caba55140211544f61c54&query=${input}`;
-   const response = await fetch(search_url);
-   const data = await response.json();
-   displayMovies(data.results);
-   console.log(data.results);
-   return data.results;
+        const search_url = `https://api.themoviedb.org/3/search/movie?api_key=18268bc1582caba55140211544f61c54&query=${input}`;
+        const response = await fetch(search_url);
+        const data = await response.json();
+        if (data.results.forEach((result) => {
+            const {backdrop_path} = result;
+        })) {
+            alert('Not Found:', result);
+        } else {
+        displayMovies(data.results);
+        console.log(data);
+        loadingImage.classList.add('hidden'); 
+        return data.results;
+        }   
 }
 
 
 function displayMovies (movies) {  
-    
     let movieContainer = ''; 
     movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview} = movie;   
+        const { title, poster_path, vote_average, overview, backdrop_path} = movie;   
                      movieContainer +=`
                      <div class="wrap">
                          <div class="output bg-white  shadow-2xl rounded-2xl p-2">
